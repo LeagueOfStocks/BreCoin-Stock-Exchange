ce7d4a1ad2e53d728311db4fe4a9f17f80ce307f
Muhammad Huzefa Ali <leagueofstocks@gmail.com>
2025-03-01 22:18:11 -0700

    Update

--- backend/api/main.py
+++ backend/api/main.py
@@ -15,8 +15,8 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
+    allow_origins=["http://localhost:3000", "https://brecoin-stock-exchange.vercel.app"],
+     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
